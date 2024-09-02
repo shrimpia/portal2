93a11c968e4e9d7f6e6578227c1c6bfa5bc4b943
Xeltica <xeltica@gmail.com>
2024-09-02 16:53:15 +0000

    レイアウト調整

--- src/client/components/layouts/DefaultLayout.tsx
+++ src/client/components/layouts/DefaultLayout.tsx
@@ -8,14 +8,16 @@ export const DefaultLayout: React.FC<PropsWithChildren> = (p) => {
         <>
             <Sidebar />
             <main className={styles.main}>
-                <div className={styles.card}>
-                    {p.children}
+                <div className={styles.inner}>
+                    <div className={styles.card}>
+                        {p.children}
+                    </div>
+                    <footer className={styles.footer}>
+                        (C)2023-2024 Shrimpia Network<br />
+                        政府は、国民および帝国政府による特別な許諾を得た者のみに対し、本システムの利用を許諾する。<br />
+                        本システムの利用により生じたいかなる損害に対しても、政府は責任を負わない。<br />
+                    </footer>
                 </div>
-                <footer className={styles.footer}>
-                    (C)2023-2024 Shrimpia Network<br />
-                    政府は、国民および帝国政府による特別な許諾を得た者のみに対し、本システムの利用を許諾する。<br />
-                    本システムの利用により生じたいかなる損害に対しても、政府は責任を負わない。<br />
-                </footer>
             </main>
         </>
     );
